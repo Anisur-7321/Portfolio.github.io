69534cd3e0ff054ca9a6874fd186a56cd9c59a65
Rabbi Hossain <rabbihossainlhp@gmail.com>
2024-12-15 23:37:52 +0600

    small respnsive nav

---
+++ script.js
@@ -0,0 +1,9 @@
+//get item first...
+let navBar = document.querySelector(".navber");
+let menueIcon = document.querySelector("#menu-icon");
+
+
+menueIcon.addEventListener("click",()=>{
+    navBar.classList.toggle("toggleNav");   
+})
+
